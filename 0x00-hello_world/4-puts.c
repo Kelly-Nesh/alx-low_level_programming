8d383f99d7a2806150e36b3b2a5977149b04c8cc
Nesh <cs.kelly.nesh@gmail.com>
2022-09-08 02:51:50 -0700

    checking for working scripts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*include header file
+ * use the main function below
+ * Puts does as its name suggests..put...
+ * returns integer 0 meaning success
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}

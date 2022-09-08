2c144eb09db36002fabab464d725c50c59c90eeb
Nesh <cs.kelly.nesh@gmail.com>
2022-09-08 02:59:12 -0700

    betty errors :(

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-/*include header file
+/*
+ * include header file
  * main - is the function below and is mandatory for all c programs;maybe?
  * Puts does as its name suggests..put...
  * returns integer 0 meaning success

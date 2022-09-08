25b1a94278585cd3517e795f9b780776b0c85749
Nesh <cs.kelly.nesh@gmail.com>
2022-09-08 03:21:54 -0700

    i hope were in good terms betty :)

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description: prints the statement in brackets
+ * Return: 0 if the program is successful else; non-zero value
+ */
+int main(void)
+
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

0330267027ef451b536d737a6c0b1a536ba36bf8
Nesh <cs.kelly.nesh@gmail.com>
2022-09-09 19:24:03 +0300

    task 5 first comm

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the function to be executed
+ * declare an int
+ * loops and print output
+ * Return:zero if successful else non-zero
+ */
+
+int main(void)
+{
+int no;
+for (no = 48; no < 58; no++)
+	putchar(no);
+putchar("\n");
+return (0);
+}

6258ac7516e73bfb32261ee4c6424397e7fbee12
Nesh <cs.kelly.nesh@gmail.com>
2022-09-09 18:33:13 +0300

    task 5 first comm

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the function to be executed
+ * declare an int
+ * loops and print output
+ * Return:zero if successful else non-zero
+ */
+
+int main(void)
+{
+int no;
+for (no = 0; no < 10; no++)
+	printf("%d", no);
+printf('\n');
+return (0);
+}
+

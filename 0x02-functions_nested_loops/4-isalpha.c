8532a9218fabe4fb5348028d416d442e2863d87d
Nesh <cs.kelly.nesh@gmail.com>
2022-09-14 11:43:45 -0700

    betty manenoz

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
-/**_isalpha - returns 1 or 0
- * return value is 1 or zero
+/**
+ * _isalpha - a function for return values 1 or 0
+ * @n : is a variable integer
+ * Return: value is 1 or zero
  */
 int _isalpha(int n)
 {
-	if((n >= 'a' && n <= 'z') ||
+	if ((n >= 'a' && n <= 'z') ||
 		(n >= 'A' && n <= 'Z'))
 		return (1);
 	else

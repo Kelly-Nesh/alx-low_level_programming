ba3d22ebb52a4d743fb5b7136290b86b61afaec3
Nesh <cs.kelly.nesh@gmail.com>
2022-09-14 11:27:06 -0700

    1-alphabet.c correction #betty errors

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,12 @@
  * print_alphabet - prints alphabets
  * using variable a to z
  * no return value specified
- * /
+ */
 
 void print_alphabet(void)
 {
 char atoz;
-for(atoz = 'a'; atoz <= 'z'; atoz++)
+for (atoz = 'a'; atoz <= 'z'; atoz++)
 	_putchar(atoz);
 _putchar('\n');
 }

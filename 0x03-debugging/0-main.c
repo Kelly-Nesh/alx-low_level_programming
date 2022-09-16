770013a4b47d4b34ab649b05107a67e28c9e9106
Nesh <cs.kelly.nesh@gmail.com>
2022-09-15 19:09:19 -0700

    integer variables

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * main - void input function
- * Return: zero value
- * @n: variable integer
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
 {
-	int n;
+	int i;
 
-	n = 0;
-	positive_or_negative(n);
+	i = 98;
+	positive_or_negative(i);
 
 	return (0);
 }

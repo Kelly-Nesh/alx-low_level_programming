5db8b79a58cd1626efe041d6148cabdd251f0e3c
Nesh <cs.kelly.nesh@gmail.com>
2022-09-19 19:28:52 -0700

    segmentation

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,12 @@ char *_strcpy(char *dest, const char *src)
 {
 	int n = 0;
 
-	while (src[n])
+	while (n >= 0)
 	{
-		dest[n] = src[n];
-		n++
+		*(dest + n) = *(src + n);
+		if (*(src + n ) == '\0')
+			break;
+		n++;
 	}
 	return (dest);
 }

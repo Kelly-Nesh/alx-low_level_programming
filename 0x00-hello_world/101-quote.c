b3908fd1d375a27775a27ad2ce1cae45d40ebf84
Nesh <cs.kelly.nesh@gmail.com>
2022-09-27 02:30:00 -0700

    retry success

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <unistd.h>
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * fprintf: used for stderr
+ * Return: one
+ */
+
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }

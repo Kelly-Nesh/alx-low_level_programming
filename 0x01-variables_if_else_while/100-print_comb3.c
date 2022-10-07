d4d7cf25ec04f1908618ab7ad39eddd9295d3693
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2022-10-07 04:03:47 +0300

    advanced trials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,20 @@
 int main(void)
 {
 int i, fd, ld;
-for (i = 0; i < 100; i++){
+
+for (i = 0; i < 100; i++)
+{
 	fd = ((i / 10)+ '0');
 	ld = ((i % 10)+ '0');
-	if (fd < ld){
+	if (fd < ld)
+	{
 	putchar(fd);
 	putchar(ld);
 	putchar(',');
 	putchar(' ');
 	}
-}putchar('\n');
+}
+putchar('\n');
 return (0);
 }
 

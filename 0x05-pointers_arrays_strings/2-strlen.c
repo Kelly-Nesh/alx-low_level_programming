ea0646953d8cf49170ee3e1939e9244ac5ac3989
Nesh <cs.kelly.nesh@gmail.com>
2022-10-14 00:02:39 -0700

    again

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-
-/**
- * main - gives length of a string
- * Return: string legth
- */
-
-int _strlen(char *);
-int main(void)
-{
-	char *str;
-	int len;
-	str = "Holberton!";
-	len = _strlen(str);
-	printf("%d\n", len);
-	return (0);
-}
 /**
  * _strlen - called by main
  * @s: pointer

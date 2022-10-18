2bb31873e67d1169c0cbb5363c9b271350710519
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2022-10-18 14:34:28 +0300

    natural numbers 001

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,25 +4,24 @@
  * main - prints sum of multiples of 3 and 5 below 1024
  * Return: zero
  */
+
 int main(void)
 {   
-    int i,j,k,sum;
-    i=0;
-    sum = 0;
+	int j,k,sum;
+	int i = 0;
+	sum = 0;
 
-    for (i=0; j < 1023; i++)
-    {
-        j = i*3;
-        sum += j;
-        printf("%d, ", j);
-    }
-    for (i=0; k < 1020; i++)
-    {
-        k = i*5;
-        sum += k;
-        printf("%d, ", k);
-    }
-    printf("\n%d\n", sum);
-    
-    return (0);
+	while (j < 1023 && k < 1020)
+	{
+		j = i*3;
+		sum += j;
+		printf("%d, ", j);
+		k = i*5;
+		sum += k;
+		printf("%d, ", k);
+		i++;
+	}
+	printf("\n%d\n", sum);
+	
+	return (0);
 }

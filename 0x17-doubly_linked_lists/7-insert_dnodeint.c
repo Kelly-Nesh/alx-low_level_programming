6d14f87b9af3dfd4262e49ddc044b667d8a623a9
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-10-30 09:08:36 +0300

    re-up marks

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -12,36 +12,30 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp, *new;
-	unsigned int count = 1;
+	dlistint_t *tmp = *h, *new;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
-	if ((!new) || (!(*h) && idx > 0))
+	if (new == NULL)
 		return (NULL);
-	temp = *h;
-	if (!temp)
-	{
-		return (add_dnodeint_end(h, n));
-	}
+
 	new->n = n;
-	if (idx == 0)
-	{
-		temp->prev = new;
-		new->next = temp;
-		new->prev = NULL;
-		*h = new;
-		return (new);
-	}
-	while (count < idx)
-	{
-		if (!temp)
-			return (NULL);
-		temp = temp->next;
-		count++;
-	}
-	new->next = temp->next;
-	temp->next = new;
-	temp->next->prev = new;
-	new->prev = temp;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
 	return (new);
 }

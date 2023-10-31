5e71799e7f6164a72f0b536120cce093930af668
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-10-31 15:39:19 +0300

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Contains a function def island_perimeter(grid): that returns the
+perimeter of the island described in grid:
+"""
+
+
+def get_width(grid):
+    """
+    returns the width of the island described in grid:
+    """
+    width = []
+    count = 0
+    for row in range(len(grid)):
+        for cell in range(len(grid[row])):
+            if grid[row][cell] == 1:
+                count += 1
+        width.append(count)
+        count = 0
+    width.sort()
+    return width[-1]
+
+def get_height(grid):
+    """
+    returns the height of the island described in grid:
+    """
+    height = []
+    count = 0
+    h = 0
+    for row in range(len(grid[0])):
+        for cell in grid:
+            count += cell[h]
+        h += 1
+        height.append(count)
+        count = 0
+    height.sort()
+    return height[-1]
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island described in grid:
+    """
+    return (get_width(grid) + get_height(grid)) * 2
